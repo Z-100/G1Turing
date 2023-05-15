7ebe1ae8495d197eeb5952181fd737734ae36423
Marvin <marvin131t@gmail.com>
2023-05-15 13:52:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# G1Turing
+
+The _Turing-series_ is a series of AI-projects aimed to do some pretty cool stuff (in the future, maybe).
+The _G1Turing_ repository - _G1_ meaning generation 1 - includes _learning_, _prototyping_ & _doing dumb
+stuff_ with AI. Or something like that.
+
+## G2Turing
+
+[404](https://github.com/ZE-100/G2Turing)
---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "com.z100.g1turing"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
